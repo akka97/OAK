a5bd56b5ac1a24c75ea97945ef08d4ecfe8f8ae2
Frensi <user@Users-MacBook.local>
2023-10-19 12:55:24 +0200

    fotot

--- src/db/gyms.js
+++ src/db/gyms.js
@@ -6,6 +6,14 @@ import perfect from "../images/perfect.jpg";
 import lake from "../images/lake.jpg";
 import fitalb from "../images/fitalb.jpg";
 import angels from "../images/angels.jpg";
+import expres from "../images/fitexpres.jpeg";
+import fit21 from "../images/crosfit21.jpeg";
+import alfa from "../images/alfa.jpeg";
+import zorba from "../images/zorba.jpeg";
+import stamina from "../images/stamina.jpeg";
+import limitles from "../images/limitless.jpeg";
+import arena from "../images/arena.jpeg";
+import magnet from "../images/magnet.jpeg";
 
 const db_data = [
     {
@@ -39,7 +47,39 @@ const db_data = [
     {
         src: angels,
         name: "Angel's Fitness Gym",
-    }
+    },
+    {
+        src: expres,
+        name: "Fit Express Tirana",
+    },
+    {
+        src: fit21,
+        name: "Fit 21shi Athlete",
+    },
+    {
+        src: alfa,
+        name: "Alpha CrossFit Tirana ",
+    },
+    {
+        src: zorba,
+        name: "ZORBA Fitness Club",
+    },
+    {
+        src: stamina,
+        name: "Crossfit stamina",
+    },
+    {
+        src: limitles,
+        name: "Limitless Fitness Tirana",
+    },
+    {
+        src: arena,
+        name: "Arena Boxing and Fitness",
+    },
+    {
+        src: magnet,
+        name: "Magnet fit and gym",
+    },
 
 
 ]
