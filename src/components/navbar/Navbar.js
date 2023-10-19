f7539025b6fa6fa798df3f7499930b1b210788b2
akka97 <akapellari@yahoo.com>
2023-10-19 13:19:19 +0200

    favicon

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -7,6 +7,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
 import MyModal from "../reusable-components/modal";
+import logooak from "../../images/logo oak.png";
 
 const NavScrollExample = () => {
 
@@ -18,7 +19,9 @@ const NavScrollExample = () => {
         <>
             <Navbar expand="lg" className="bg-body-tertiary">
                 <Container fluid>
-                    <Navbar.Brand href="#">LOGOOOOOOO</Navbar.Brand>
+                    <Navbar.Brand>
+                        <img src={logooak} />
+                    </Navbar.Brand>
                     <Navbar.Toggle aria-controls="navbarScroll" />
                     <Navbar.Collapse id="navbarScroll">
                         <Nav
