9dd2092b8f8a2dcb075ee8ce639591db5adb9a6a
Frensi <user@Users-MacBook.local>
2023-10-19 10:43:33 +0200

    add

--- src/db/gyms.js
+++ src/db/gyms.js
@@ -1,13 +1,15 @@
 import gym_img from "../images/puzzle.png";
+import nobis2 from "../images/nobis2.png"
+import repeat from "../images/REPEAT.png"
 
 const db_data = [
     {
-        src: gym_img,
-        name: "name gym",
+        src: nobis2,
+        name: "Nobis Wellness Center",
     },
     {
-        src: gym_img,
-        name: "name gym",
+        src: repeat,
+        name: "Repeat Fitness Tirana",
     },
     {
         src: gym_img,
