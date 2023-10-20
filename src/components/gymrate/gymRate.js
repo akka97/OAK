fe9090aeedec32f81d398f5d0a6b59b996fd07aa
Frensi <user@Users-MacBook.local>
2023-10-20 10:31:41 +0200

    adding

--- src/components/gymrate/gymRate.js
+++ src/components/gymrate/gymRate.js
@@ -3,28 +3,83 @@ import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 import Row from 'react-bootstrap/Row';
 import db_rate from "../../db/gymsRate";
-import './gymRate.css'
+import './gymRate.css';
+import alfa from "../../images/alfa.jpeg"
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Placeholder from 'react-bootstrap/Placeholder';
 
 
+//
+//function RateExample() {
+//    return (
+//        <Container>
+//            <Row>
+//                {
+//                    db_rate.map((el, index) => {
+//                        return (
+//                            <Col xs={6} md={4}>
+//                                <h5>{el.name}</h5>
+//                                <Image src={el.src} thumbnail />
+//                            </Col>
+//                        )
+//                    })
+//                }
+//            </Row>
+//        </Container>
+//    );
+//}
+//
+//export default RateExample;
 
-
-function RateExample() {
+function CardExample() {
     return (
-        <Container>
-            <Row>
-                {
-                    db_rate.map((el, index) => {
-                        return (
-                            <Col xs={6} md={4}>
-                                <h5>{el.name}</h5>
-                                <Image src={el.src} thumbnail />
-                            </Col>
-                        )
-                    })
-                }
-            </Row>
-        </Container>
-    );
+        <div className="d-flex justify-content-around">
+            <Card style={{ width: '17rem' }}>
+                <Card.Img variant="top" src={alfa} />
+                <Card.Body >
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '17rem' }}>
+                <Card.Img variant="top" src={alfa} />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '17rem' }}>
+                <Card.Img variant="top" src={alfa} />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '17rem' }}>
+                <Card.Img variant="top" src={alfa} />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+            </div>
+    )
 }
-
-export default RateExample;
+export default CardExample;
