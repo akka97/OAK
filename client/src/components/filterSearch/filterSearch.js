4106baba7cdbc2f2dc42b1d13caafda5ba79ab45
Frensi <user@Users-MacBook.local>
2023-11-09 11:38:15 +0100

    add

--- client/src/components/filterSearch/filterSearch.js
+++ client/src/components/filterSearch/filterSearch.js
@@ -1,12 +1,25 @@
 import TextField from '@mui/material/TextField';
+import React, { useState } from 'react';
+import db_data from '../../db/gyms';
 
 const FilterSearch = () => {
+    const [searchText, setSearchText] = useState('');
+
+    const handleSearchChange = (event) => {
+        const searchText = event.target.value.toLowerCase();
+
+        const filteredGyms = db_data.filter(gym =>
+            gym.name.toLowerCase().includes(searchText)
+        );
+
+        console.log(filteredGyms);
+    };
 
     return (
         <>
-            <TextField id="outlined-basic" className="search-input" label="Search Gyms..." variant="outlined" />
+            <TextField id="outlined-basic" className="search-input" label="Search Gyms...  " variant="outlined" onChange={handleSearchChange} />
         </>
-    )
-}
+    );
+};
 
 export default FilterSearch;
